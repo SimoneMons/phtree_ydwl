336ff9a892810ad7eae7f112ead99a4a9c851d63
Mons <smontefiori@gmail.com>
2020-09-09 17:23:55 +0200

    commit_09092020

--- run_trheads.py
+++ run_trheads.py
@@ -1,6 +1,7 @@
 from PyQt5.QtCore import pyqtSignal, QThread
 import youtube_dl
-import os
+import os, sys
+import re
 
 # Global variables
 SAVE_PATH = os.path.expanduser('~/Downloads')
@@ -16,21 +17,131 @@ ydl_audio = {
     'outtmpl': SAVE_PATH + '/_audio/%(title)s.%(ext)s',
 }
 
-class DownloadVideo(QThread):
+def resource_path(relative_path):
+    if hasattr(sys, '_MEIPASS'):
+        print(' ')
+        return os.path.join(sys._MEIPASS, relative_path)
+    return os.path.join(os.path.abspath("venv"), relative_path)
+
+class DownloadData(QThread):
     signal = pyqtSignal('PyQt_PyObject')
 
-    def __init__(self, dwl_link, video_id_list, parent=None):
-        super(DownloadVideo, self).__init__(parent)
-        self.dwl_link = dwl_link
+    def __init__(self, video_id_list, dwl_choice, parent=None):
+        super(DownloadData, self).__init__(parent)
         self.video_id_list = video_id_list
+        self.dwl_choice = dwl_choice
 
 
     def run(self):
-        # Download video
-        with youtube_dl.YoutubeDL(ydl_video) as ydlvideo:
-            ydlvideo.download([self.dwl_link])
+        # Download videos
+
+        # .exe
+        # ffmpeg_path = "./ffmpeg/ffmpeg.exe"
+
+
+        # pycharm exe
+        ffmpeg_path = ".\\ffmpeg\\ffmpeg.exe"
+
+        # create directories
+        video_directory = os.path.join(SAVE_PATH, '_video')
+
+        if not os.path.isdir(video_directory):
+            os.mkdir(video_directory)
+
+        audio_directory = os.path.join(SAVE_PATH, '_audio')
+
+        if not os.path.isdir(audio_directory):
+            os.mkdir(audio_directory)
+
+        video_path = os.path.expanduser('~\Downloads\_video')
+        audio_path = os.path.expanduser('~\Downloads\_audio')
+
+        # Only music
+        if self.dwl_choice == 'Only Music':
+            #self.l.setText("Downloading Music")
+            with youtube_dl.YoutubeDL(ydl_audio) as ydlaudio:
+                for id_video in self.video_id_list:
+                    ydlaudio.download([id_video])
+
+            # convert to webm to mp3
+            arr_webm = [x for x in os.listdir(audio_path) if x.endswith(".webm") or x.endswith(".m4a")]
+            print(arr_webm)
+
+            #self.tab1.textmessage.setText('Creating MP3 files')
+
+            for file in arr_webm:
+                filename_old = file
+                # filename_new_ap = re.sub(r"\s+", '_', filename_old)
+                # filename_new = re.sub(r"&+", '_', filename_new_ap)
+
+                filename_new = filename_old.replace("&", "_")
+
+                filename_new = filename_new.replace(" ", "_")
+
+                os.rename(audio_path + '\\' + filename_old, audio_path + '\\' + filename_new)
+                print('old ', filename_old)
+                print('new ', filename_new)
+                # pycharm exe
+                os.system(ffmpeg_path + ' -i ' + audio_path + '\\' + filename_new +
+                          ' -vn -ar 44100 -ac 2 -ab 192k -f mp3 ' + audio_path + '\\' + filename_new[:-5] + '.mp3')
+
+                # exe
+                # os.system(resource_path(ffmpeg_path) + ' -i ' + audio_path + '\\' + filename_new +
+                #          ' -vn -ar 44100 -ac 2 -ab 192k -f mp3 ' + audio_path + '\\' + filename_new[:-5] + '.mp3')
+
+                os.remove(audio_path + '\\' + filename_new)
+
+        # Only video
+        elif self.dwl_choice == 'Only Video':
+            #self.tab1.textmessage.setText('Downloading Videos')
+            with youtube_dl.YoutubeDL(ydl_video) as ydlvideo:
+                for id_video in self.video_id_list:
+                    ydlvideo.download([id_video])
+
+        else:
+            #self.tab1.textmessage.setText('Downloading Videos')
+            with youtube_dl.YoutubeDL(ydl_video) as ydlvideo:
+                for id_video in self.video_id_list:
+                    ydlvideo.download([id_video])
+
+            # Create audio files
+            #self.tab1.textmessage.setText('Creating MP3 files')
+
+            for file in os.listdir(video_path):
+                if file.endswith(".mp4"):
+                    filename = file
+                    filename_new = re.sub(r"\s+", '_', filename)
+                    # Rename file
+
+                    filename_new_mp3 = filename_new[:-4] + '.mp3'
+
+                    old_file = os.path.join(video_path, filename)
+
+                    new_file = os.path.join(video_path, filename_new)
+
+                    if filename_new not in os.listdir(video_path):
+                        os.rename(old_file, new_file)
+
+                    video_name = '\\' + filename_new
+                    audio_name = '\\' + os.path.splitext(video_name)[0] + '.mp3'
+
+                    print(video_name)
+                    print(audio_name)
+
+                    # Generate audio
+                    # pycharm exe
+                    print('Holaaaaaaaaaaaaaaaaaaa sssssssss')
+                    if filename_new_mp3 not in os.listdir(audio_path):
+                        os.system(ffmpeg_path + ' -i ' + resource_path(video_path + video_name) +
+                                  ' -vn -ar 44100 -ac 2 -ab 192k -f mp3 ' + resource_path(audio_path + audio_name))
+
+                    # .exe
+                    # if filename_new_mp3 not in os.listdir(audio_path):
+                    #    os.system(resource_path(ffmpeg_path) + ' -i ' + resource_path(video_path + video_name) +
+                    #              ' -vn -ar 44100 -ac 2 -ab 192k -f mp3 ' + resource_path(audio_path + audio_name))
 
         self.signal.emit('Holaaaaaaa')
+
 
 
 
--- tabs.py
+++ tabs.py
@@ -5,7 +5,10 @@ import youtube_dl
 
 from PyQt5.QtWidgets import *
 from PyQt5 import QtGui
-from run_trheads import DownloadVideo
+from run_trheads import DownloadData
+
+from youtubesearchpython import SearchVideos, SearchPlaylists
+import json
 
 # Global variables
 SAVE_PATH = os.path.expanduser('~/Downloads')
@@ -39,29 +42,38 @@ class youdwnl_tabs(QWidget):
         self.tabs = QTabWidget()
         self.tab1 = QWidget()
         self.tab2 = QWidget()
+        self.tab3 = QWidget()
         # self.tabs.setFixedSize(600, 400)
 
         # Add tabs
-        self.tabs.addTab(self.tab1, "Tab 1")
-        self.tabs.addTab(self.tab2, "Tab 2")
+        self.tabs.addTab(self.tab1, "Link")
+        self.tabs.addTab(self.tab2, "Search")
+        self.tabs.addTab(self.tab3, "Info")
 
+        self.tab1UI()
+        self.tab2UI()
+        self.tab3UI()
+
+
+    def tab1UI(self):
         # Create first tab
         self.tab1.layout = QVBoxLayout(self)
         self.tab1.setLayout(self.tab1.layout)
         # self.tab1.setStyleSheet("background-image: url(./images/istockphoto-1172479732-170667a.jpg)")
 
-        # Information
+        # Information tab 1
         self.tab1.label = QLabel(self.tab1)
         self.tab1.label.setStyleSheet('color: black')
         self.tab1.label.setFont(QtGui.QFont('Arial', 10))
         self.tab1.label.setText("Insert the downloading video link or playlist")
         self.tab1.label.setGeometry(50, 20, 300, 20)
 
-        # Download link
+        # Download link tab 1
         self.tab1.linktextbox = QLineEdit(self.tab1)
         self.tab1.linktextbox.move(50, 40)
         self.tab1.linktextbox.resize(300, 20)
         self.tab1.linktextbox.returnPressed.connect(self.oh_no)
+
 
         # check box playlist
         self.tab1.boxpl = QCheckBox("Download related videos", self.tab1)
@@ -74,12 +86,38 @@ class youdwnl_tabs(QWidget):
         self.tab1.combo_choice.addItem("Video & Music")
         self.tab1.combo_choice.addItem("Only Music")
         self.tab1.combo_choice.addItem("Only Video")
-        self.tab1.combo_choice.setStyleSheet("QComboBox"
-                                        "{"
-                                        "background-color: white;"
-                                        "}")
+        #self.tab1.combo_choice.setStyleSheet("QComboBox"
+        #                                "{"
+        #                                "background-color: white;"
+        #                                "}")
         self.tab1.combo_choice.move(70, 80)
         self.tab1.combo_choice.resize(100, 20)
+
+        # Create dwl button
+        self.tab1.dwl = QPushButton('Download', self.tab1)
+        self.tab1.dwl.setToolTip('Click here to download your video')
+        self.tab1.dwl.setFont(QtGui.QFont('Arial', 9))
+        #self.tab1.dwl.setStyleSheet("QPushButton"
+        #                       "{"
+        #                       "background-color: #C0C0C0; border-radius: 10px;"
+        #                       "}")
+        self.tab1.dwl.setGeometry(90, 125, 100, 25)
+        # self.dwl.move(90, 125)
+        self.tab1.dwl.clicked.connect(self.oh_no)
+
+
+        # Create close button
+        self.tab1.cls = QPushButton('Clear data', self.tab1)
+        self.tab1.cls.setToolTip('Click here to claer the data')
+        self.tab1.cls.setFont(QtGui.QFont('Arial', 9))
+        self.tab1.cls.setGeometry(210, 125, 100, 25)
+        #self.tab1.cls.setStyleSheet("QPushButton"
+        #                       "{"
+        #                       "background-color: #C0C0C0; border-radius: 10px;"
+        #                       "}")
+        # self.cls.move(210, 125)
+        self.tab1.cls.clicked.connect(self.clear_fn)
+
 
         # Message text
         self.tab1.textmessage = QLabel(self.tab1)
@@ -87,20 +125,110 @@ class youdwnl_tabs(QWidget):
         self.tab1.textmessage.setText("Ready to download")
         self.tab1.textmessage.setGeometry(100, 165, 300, 15)
 
+        # Information
+        self.tab1.instructions1 = QLabel(self.tab1)
+        self.tab1.instructions1.setStyleSheet('color: black')
+        self.tab1.instructions1.setText("Check your files in:")
+        self.tab1.instructions1.setFont(QtGui.QFont('Arial', 10))
+        self.tab1.instructions1.setGeometry(30, 210, 300, 20)
+
+        # Information
+        self.tab1.instructions2 = QLabel(self.tab1)
+        self.tab1.instructions2.setStyleSheet('color: black')
+        self.tab1.instructions2.setFont(QtGui.QFont('Arial', 10))
+        self.tab1.instructions2.setText("\Downloads\_video (MP4)")
+        self.tab1.instructions2.setGeometry(30, 240, 300, 20)
+
+        # Information
+        self.tab1.instructions3 = QLabel(self.tab1)
+        self.tab1.instructions3.setStyleSheet('color: black')
+        self.tab1.instructions3.setFont(QtGui.QFont('Arial', 10))
+        self.tab1.instructions3.setText("\Downloads\_audio (MP3)")
+        self.tab1.instructions3.setGeometry(30, 270, 300, 20)
+
+        # Information
+        self.tab1.instructions4 = QLabel(self.tab1)
+        self.tab1.instructions4.setStyleSheet('color: black')
+        self.tab1.instructions4.setFont(QtGui.QFont('Arial', 7))
+        self.tab1.instructions4.setText("Enjoy, by Mons 2020")
+        self.tab1.instructions4.setGeometry(385, 380, 300, 20)
+
+        # Information Search tab 2
+        self.tab2.label = QLabel(self.tab2)
+        self.tab2.label.setStyleSheet('color: black')
+        self.tab2.label.setFont(QtGui.QFont('Arial', 10))
+        self.tab2.label.setText("Search your music to download")
+        self.tab2.label.setGeometry(50, 20, 300, 20)
+
+    def tab2UI(self):
+        # Search tab 2
+        self.tab2.searchtextbox = QLineEdit(self.tab2)
+        self.tab2.searchtextbox.move(50, 40)
+        self.tab2.searchtextbox.resize(300, 20)
+        self.tab2.searchtextbox.returnPressed.connect(self.oh_no_search)
+
+        # Play list or single videos
+        self.tab2.dwnl_choice = QComboBox(self.tab2)
+        self.tab2.dwnl_choice.addItem("Single Videos")
+        self.tab2.dwnl_choice.addItem("Playlist")
+        # self.tab1.combo_choice.setStyleSheet("QComboBox"
+        #                                "{"
+        #                                "background-color: white;"
+        #                                "}")
+        self.tab2.dwnl_choice.move(390, 40)
+        self.tab2.dwnl_choice.resize(100, 20)
+
+        # Search result
+        self.tab2.title = QPlainTextEdit(self.tab2)
+        self.tab2.title.setGeometry(50, 70, 300, 350)
+
+    def tab3UI(self):
+        # Information
+        self.tab3.info = QPlainTextEdit(self.tab3)
+        self.tab3.info.setGeometry(50, 70, 470, 350)
+        self.tab3.info.insertPlainText('With this program you can download videos and music from Youtube' + "\n" + "\n")
+        self.tab3.info.insertPlainText(
+            'Simple link example:' + "\n" + "\n")
+        self.tab3.info.insertPlainText(
+            'https://www.youtube.com/watch?v=UojBaKX5Vz4' + "\n" + "\n" + "\n")
+        self.tab3.info.insertPlainText(
+            'Playlist link example:' + "\n" + "\n")
+        self.tab3.info.insertPlainText(
+            'https://www.youtube.com/playlist?list=RDEMDHx1mzcs_wPqWOntgHDScQ' + "\n" + "\n" + "\n")
+        self.tab3.info.insertPlainText(
+            'List of video link example:' + "\n" + "\n")
+        self.tab3.info.insertPlainText(
+            'https://www.youtube.com/watch?v=0_U4D3Wy-7k&list=RDEMDHx1mzcs_wPqWOntgHDScQ&index=5' + "\n" + "\n" )
+        self.tab3.info.insertPlainText(
+            "In this case, if you want to download all of them, mark the check box: 'Download all related videos'" + "\n" + "\n")
+
         # Add tabs to widget
         self.layout.addWidget(self.tabs)
         self.setLayout(self.layout)
 
     def finished(self):
-        self.tab1.textmessage.setText('Video download completed')
+        self.tab1.textmessage.setText('Data download completed')
+        print('Finito')
+
+    def clear_fn(self):
+        self.tab1.linktextbox.setText('')
+        self.tab1.boxpl.setChecked(False)
 
     def oh_no(self):
         # Link to download
-        dwl_link = self.tab1.linktextbox.text()
+        dwl_ini_link = self.tab1.linktextbox.text()
+
+        # Download options: Only Video, Only Music, Video & Music
+        dwl_choice = str(self.tab1.combo_choice.currentText())
+
+        # Related files of a list
+        dwl_related = self.tab1.boxpl.checkState()
+
+        print(dwl_ini_link, '\n', dwl_related, '\n', dwl_choice)
 
         # Validate the link
         try:
-            request = requests.get(dwl_link)
+            request = requests.get(dwl_ini_link)
             print('Web site exists')
         except:
             print('Web site does not exist')
@@ -108,36 +236,76 @@ class youdwnl_tabs(QWidget):
         # id of videos to download
         video_id_list = []
 
-
-        if 'playlist' in dwl_link:
+        if 'playlist' in dwl_ini_link:
+            video_id_list.append(dwl_ini_link)
             print('It is a playlist')
 
         # validate if the link is a list of videos
-        elif 'watch?v=' and '&list' in dwl_link:
-            # Related videos to download
-            r = requests.get(dwl_link)
-            page_source = r.text
-            for m in re.finditer('":{"url":"/watch?', page_source):
-                video_id = page_source[m.start() + 19:m.end() + 90]
-                print('videossssss', video_id)
-                if 'index=' in video_id:
-                    video_id = video_id.split('\\')[0]
-                    if video_id not in video_id_list:
-                        video_id_list.append(video_id)
+        elif 'watch?v=' and '&list' in dwl_ini_link:
+            if dwl_related == 2:
+                # Related videos to download
+                r = requests.get(dwl_ini_link)
+                page_source = r.text
+                for m in re.finditer('":{"url":"/watch?', page_source):
+                    video_id = page_source[m.start() + 19:m.end() + 90]
+                    print('videossssss', video_id)
+                    if 'index=' in video_id:
+                        video_id = video_id.split('\\')[0]
+                        if 'https://www.youtube.com/watch?v=' + video_id not in video_id_list:
+                            video_id_list.append('https://www.youtube.com/watch?v=' + video_id)
+            elif dwl_related == 0:
+                # Format link to download
+                result = dwl_ini_link.find('list')
+                # print('link result', result)
+                dwl_end_link = dwl_ini_link[0:result - 1]
+                print('linkkk0 ', dwl_end_link)
+                video_id_list.append(dwl_end_link)
+        else:
+            video_id_list.append(dwl_ini_link)
 
-            # Original link
-            result = dwl_link.find('list')
-            # print('link result', result)
-            dwl_link = dwl_link[0:result]
-            print('linkkk0 ', dwl_link)
+        print(video_id_list)
+
 
         # Define the thread
-        self.dwnl_thread = DownloadVideo(dwl_link, video_id_list)  # Any other args, kwargs are passed to the run function
+        self.dwnl_thread = DownloadData(video_id_list, dwl_choice)  # Any other args, kwargs are passed to the run function
         self.dwnl_thread.signal.connect(self.finished)
+
 
         # Execute the thread
         self.dwnl_thread.start()
-        self.tab1.textmessage.setText('Downloading video')
+        self.tab1.textmessage.setText('Downloading data')
+
+
+    def oh_no_search(self):
+        # Reset data result list
+        self.tab2.title.clear()
+
+        # Data to search to download
+        search_data = self.tab2.searchtextbox.text()
+
+        download_choice = str(self.tab2.dwnl_choice.currentText())
+
+        if download_choice == 'Single Videos':
+            search = SearchVideos(search_data, offset=1, mode="json", max_results=20)
+        elif download_choice == 'Playlist':
+            search = SearchPlaylists("cold play", offset = 1, mode = "json", max_results = 20)
+        else:
+            search = SearchVideos(search_data, offset=1, mode="json", max_results=20)
+
+        # print(search.result())
+
+        aaa = json.loads(search.result())
+
+        bbb = aaa['search_result']
+
+        for d in bbb:
+            print(d['id'])
+            self.tab2.title.insertPlainText(d['title'] + "\n" + "\n")
+
+        print(bbb)
+
+
+
 
 
 
